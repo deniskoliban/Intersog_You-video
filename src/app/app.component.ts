349a52844cf9ebb666c10811d80325fea1f3377a
denis.koliban <deniskoliban@gmail.com>
2019-09-21 14:57:26 +0300

    Fixed bug with a date order

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { YoutubeService } from './services/youtube.service';
+import { reverse } from 'lodash';
 
 
 @Component({
@@ -12,6 +13,7 @@ export class AppComponent implements OnInit {
   
   gapi = (<any>window).gapi;
   items = [];
+  currentDate: string;
 
   constructor(private cr: ChangeDetectorRef) {
   }
@@ -19,6 +21,18 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.gapi.load('client');
     setTimeout(this.loadClient, 150);
+    this.getCurrentDate();
+  }
+
+  getCurrentDate() {
+    let today = new Date();
+    let dd = String(today.getDate());
+    let mm = String(today.getMonth() + 1);
+    let yyyy = today.getFullYear();
+
+    this.currentDate = yyyy + '-' + mm + '-' + dd + 'T00:00:00Z';
+    console.log(this.currentDate);
+   
   }
 
   loadClient() {
@@ -27,16 +41,18 @@ export class AppComponent implements OnInit {
   }
 
   execute() {
+    
     return this.gapi.client.youtube.search.list({
       "part": "snippet",
       "maxResults": 15,
       "q": "javascript|python -basics",
       "order": "date",
-      "publishedAfter": "2019-08-04T07:55:29.000Z",
+      "publishedAfter": "2019-08-04T00:00:00Z",
+      "publishedBefore": this.currentDate
     })
       .then((response) => {
         console.log(response);
-        this.items = response.result.items;
+        this.items = reverse(response.result.items);
         console.log(response.result.items);
         this.cr.detectChanges();
       }, (err) => { console.error("Execute error", err); });
